a0da0cf31c4f2b9c8e730dfe59d7d046c3e69d19
Jerune Hardy P. Cunanan <jerunecunanan@yahoo.com>
2020-04-02 23:53:41 +0800

    Added message function

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import discord
 from dotenv import load_dotenv
 
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
@@ -13,9 +14,22 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     servers=list(client.guilds)
     members=list(client.guilds[0].members)
+    textChannels=list(client.guilds[0].text_channels)
+    
+    print(textChannels[1].id)
+   
+    for i in range(len(textChannels)): #send message to everyone
+      channelId = client.get_channel(textChannels[i].id)
+      await channelId.send('Test Message @everyone')
 
-    for x in range(len(members)):
-    	print(members[x-1])
+    # print(channels)
+# @client.event
+# async def report(server, name, *args, **kwargs):
+#     channel = discord.utils.get(server.channels, name=name, type=discord.ChannelType.text)
+#     print(channel)
+
+    # for x in range(len(members)): //members
+    # 	print(members[x-1])
     # for x in range(len(servers)):
     #  print(''+servers[x-1].name)
 
